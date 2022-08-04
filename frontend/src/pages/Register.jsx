b6877f69f359501056671c0ce2f2807b828e061b
ShivamYadav0207 <shivamyadav.0207@gmail.com>
2022-08-04 12:53:42 +0530

    register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -7,62 +7,6 @@ import { useSignupUserMutation } from '../services/appApi.js'
 
 const Register = () => {
 
-  const navigate = useNavigate();
-
-  const[name, setName] = useState('');
-  const[email, setEmail] = useState('');
-  const[password, setPassword] = useState('');
-  const [signupUser, {isLoading, error}] = useSignupUserMutation();
-
-  const[image, setImage] = useState(null);
-  const[uploadingImage, setUploadingImage] = useState(false);
-  const[imagePreview, setImagePreview] = useState(false);
-
-  function validateImage(e) {
-    const file = e.target.files[0];
-    if(file.size >= 1048576) {
-      return alert("Max Size for Images is 1MB");
-    } else {
-      setImage(file);
-      setImagePreview(URL.createObjectURL(file));
-    }
-  }
-
-  async function uploadImage() {
-    const data = new FormData();
-    data.append("file", image);
-    data.append("upload_preset", "Your Folder");
-    try {
-        setUploadingImage(true);
-        let res = await fetch("https://api.cloudinary.com/v1_1/YOUR USERNAME/image/upload", {
-            method: "post",
-            body: data,
-        });
-        const urlData = await res.json();
-        setUploadingImage(false);
-        return urlData.url;
-    } catch (error) {
-        setUploadingImage(false);
-        console.log(error);
-    }
-  };
-  
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if(!image) {
-      return alert("Please upload your profile image");
-    }
-    const url = await uploadImage(image);
-    console.log(url);
-    //register user
-    signupUser({name, email, password, image: url}).then(({data}) => {
-      if(data) {
-        console.log(data);
-        navigate("/login");
-      }
-    })
-  };
-
   return (
     <div className='login-container'>
       <div className="login-row">
