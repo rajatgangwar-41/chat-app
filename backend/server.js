2c612c5ddb549eb0e24fddcbf3b5ed5c5ca6f3c0
Rajat Gangwar <rajat.gangwar41@gmail.com>
2022-08-04 13:30:27 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
-const userRoutes = require('./routes/userRoutes');
-const Message = require("./models/messageModel");
-const User = require("./models/userModel");
 
-const rooms = ["World", "Balkans", "Europa", "Asia", "Africa", "America", "Australia", "Antarctica"];
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors());
-
-app.use('/users', userRoutes)
+const app = express(); 
 
 //Connect with DB
 dotenv.config();
@@ -28,6 +17,11 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     
 });
 
+const rooms=["miljan","ricpe"];
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
 //create server
 const server = require("http").createServer(app);
 const PORT = 5000;
@@ -36,72 +30,6 @@ const io = require("socket.io")(server, {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST']
     }
-});
-
-async function getLastMessagesFromRoom(room){
-    let roomMessages = await Message.aggregate([
-      {$match: {to: room}},
-      {$group: {_id: '$date', messagesByDate: {$push: '$$ROOT'}}}
-    ])
-    return roomMessages;
-}
-
-function sortRoomMessagesByDate(messages){
-    return messages.sort(function(a, b){
-      let date1 = a._id.split('/');
-      let date2 = b._id.split('/');
-  
-      date1 = date1[2] + date1[0] + date1[1]
-      date2 =  date2[2] + date2[0] + date2[1];
-  
-      return date1 < date2 ? -1 : 1
-    })
-}
-
-io.on('connection', (socket)=> {
-
-    socket.on('new-user', async ()=> {
-      const members = await User.find();
-      io.emit('new-user', members)
-    })
-  
-    socket.on('join-room', async(newRoom, previousRoom)=> {
-      socket.join(newRoom);
-      socket.leave(previousRoom);
-      let roomMessages = await getLastMessagesFromRoom(newRoom);
-      roomMessages = sortRoomMessagesByDate(roomMessages);
-      socket.emit('room-messages', roomMessages)
-    })
-  
-    socket.on('message-room', async(room, content, sender, time, date) => {
-      const newMessage = await Message.create({content, from: sender, time, date, to: room});
-      let roomMessages = await getLastMessagesFromRoom(room);
-      roomMessages = sortRoomMessagesByDate(roomMessages);
-      // sending message to room
-      io.to(room).emit('room-messages', roomMessages);
-      socket.broadcast.emit('notifications', room)
-    })
-  
-    app.delete('/logout', async(req, res)=> {
-      try {
-        const {_id, newMessages} = req.body;
-        const user = await User.findById(_id);
-        user.status = "Offline";
-        user.newMessages = newMessages;
-        await user.save();
-        const members = await User.find();
-        socket.broadcast.emit('new-user', members);
-        res.status(200).send();
-      } catch (e) {
-        console.log(e);
-        res.status(400).send()
-      }
-    })
-  
-  })
-
-app.get('/rooms', (req, res)=> {
-    res.json(rooms)
 });
 
 server.listen(PORT, () => {
