ad6dbd56fcf5cde55fed5f8781baa0e202853dc6
Rajat Gangwar <rajat.gangwar41@gmail.com>
2022-08-04 13:34:54 +0530

    user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+
+//Create table for user
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: [true, "Can't be blank"]},
+    email: {type: String, required: [true, "Can't be blank"], unique: true, lowercase: true, index: true,
+     validate: [isEmail, "Invalid Email"]},
+    password: {type: String, required: [true, "Can't be blank"]},
+    image: {type: String},
+    newMessages: {type: Object, default: {}},
+    status: {type: String, default: "Online"}
+}, {
+    minimize: false
+    timestamps: true
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
